be8d7df26cd34862e6a8f3543a340f64a1f8a082
Martin Algesten <martin@algesten.se>
2025-03-16 18:08:06 +0100

    x

--- src/server/test/state_cleanup.rs
+++ src/server/test/state_cleanup.rs
@@ -5,6 +5,33 @@ use crate::CloseReason;
 use super::scenario::Scenario;
 
 #[test]
+fn http10_with_keep_alive() {
+    // Create a scenario with an HTTP/1.0 request with "connection: keep-alive" header
+    let scenario = Scenario::builder()
+        .request(
+            Request::get("/path")
+                .version(Version::HTTP_10)
+                .header("connection", "keep-alive")
+                .body(())
+                .unwrap(),
+        )
+        .response(
+            Response::builder()
+                .status(200)
+                .header("content-length", "0")
+                .body(())
+                .unwrap(),
+        )
+        .build();
+
+    // Get a Reply in the Cleanup state
+    let reply = scenario.to_cleanup();
+
+    // Verify that we don't need to close the connection
+    assert!(!reply.must_close_connection());
+}
+
+#[test]
 fn reuse_connection() {
     // Create a scenario with a GET request and a response
     let scenario = Scenario::builder()
